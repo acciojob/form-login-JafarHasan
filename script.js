d6f44419cb95108529205689124e8db517c9ee83
JafarHasan <159635643+JafarHasan@users.noreply.github.com>
2024-06-18 10:30:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,22 @@
 function getFormvalue() {
     //Write your code here
+	//using getElementById
+	/*
 	var first=document.getElementById("fname").value;
 	var last= document.getElementById("lname").value;
 	var fullName=first+" "+last;
 	alert(fullName);
+	*/
+
+	//using queryselector which will return the node list
+	const inputs=document.querySelectorAll("input");
+	let first=inputs[0].value;
+	let last=inputs[1].value;
+	let fullname=first+" "+last;
+	alert(fullname);
+	
+
+	
 	
 
 }

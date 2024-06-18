b6c71665220bfdc289d8fb2038d80ea9479056d9
JafarHasan <159635643+JafarHasan@users.noreply.github.com>
2024-06-18 10:18:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
 function getFormvalue() {
     //Write your code here
+	var first=document.getElementById("fname").value;
+	var last= document.getElementById("lname").value;
+	var fullName=first+" "+last;
+	alert(fullName);
+	
 
 }
